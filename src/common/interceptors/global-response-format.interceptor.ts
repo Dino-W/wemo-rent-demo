8d6af492d0e1dd54cb979f91d47cc4f2e63f1b6d
Dino-W <gimmy820322@gmail.com>
2025-01-05 22:31:31 +0800

    Feat : 新增全域Interceptor & exception處理

---
+++ src/common/interceptors/global-response-format.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { STATUS_CODES } from 'src/common/constant/status-codes.constant';
+
+@Injectable()
+export class ResponseFormatInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: STATUS_CODES.SUCCESS,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        result: data || {},
+      })),
+    );
+  }
+}
